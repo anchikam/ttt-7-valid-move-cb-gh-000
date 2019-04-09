e9fa0802626ffae20c2e9ad07a7f98d725eeae90
Anna Manukian <amanu001@odu.edu>
2019-04-09 17:20:44 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -13,7 +13,7 @@ end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
   b = board[index]
-  if b == " " or b == "" ior b == nil
+  if b == " " or b == "" or b == nil
     false
   elsif b == "X" or b == "O"
     true
